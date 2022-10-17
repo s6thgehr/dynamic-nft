1c4a8e3515bbc935d3f121b6db0ea1456b06eec2
Thomas Gehrmann <s6thgehr@gmail.com>
2022-10-17 09:52:14 +0200

    Add randomness to BullBear contract

--- contracts/BullBear.sol
+++ contracts/BullBear.sol
@@ -14,6 +14,8 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 // This import includes functions from both ./KeeperBase.sol and
 // ./interfaces/KeeperCompatibleInterface.sol
 import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 // Dev imports. This only works on a local dev network
 // and will not work on any test or main livenets.
@@ -26,18 +28,34 @@ contract BullBear is
     ERC721Enumerable,
     ERC721URIStorage,
     Ownable,
-    KeeperCompatibleInterface
+    KeeperCompatibleInterface,
+    VRFConsumerBaseV2
 {
     event TokensUpdated(string marketTrend);
+    event RandomNFTUriRequested(uint256 requestId);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
 
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
+    enum MarketTrend {
+        BULL,
+        BEAR
+    }
+    MarketTrend public currentMarketTrend = MarketTrend.BULL;
+
     uint256 private immutable i_interval;
     uint256 private s_lastTimestamp;
     int256 private s_lastPrice;
+
     AggregatorV3Interface private immutable i_priceFeed;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
 
     // IPFS URIs for the dynamic nft graphics/metadata.
     // NOTE: These connect to my IPFS Companion node.
@@ -53,9 +71,21 @@ contract BullBear is
         "https://ipfs.io/ipfs/QmbKhBXVWmwrYsTPFYfroR2N7NAekAMxHUVg2CWks7i9qj?filename=simple_bear.json"
     ];
 
-    constructor(uint256 interval, address priceFeedAddress) ERC721("Bull&Bear", "BBTK") {
+    constructor(
+        uint256 interval,
+        address priceFeedAddress,
+        address vrfCoordinatorAddress,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) ERC721("Bull&Bear", "BBTK") VRFConsumerBaseV2(vrfCoordinatorAddress) {
         i_interval = interval;
         i_priceFeed = AggregatorV3Interface(priceFeedAddress);
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorAddress);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+
         s_lastTimestamp = block.timestamp;
         s_lastPrice = getCurrentPrice();
     }
@@ -110,25 +140,44 @@ contract BullBear is
         int256 currentPrice = getCurrentPrice();
 
         if (currentPrice > s_lastPrice) {
-            updateAllTokenUris("bull");
+            currentMarketTrend = MarketTrend.BULL;
         }
 
         if (currentPrice < s_lastPrice) {
-            updateAllTokenUris("bear");
+            currentMarketTrend = MarketTrend.BEAR;
         }
+
+        // Will revert if subscription is not set and funded.
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+
+        emit RandomNFTUriRequested(requestId);
+
+        s_lastPrice = currentPrice;
     }
 
-    function updateAllTokenUris(string memory trend) internal {
-        if (compareStrings("bear", trend)) {
+    function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomWords)
+        internal
+        override
+    {
+        if (currentMarketTrend == MarketTrend.BEAR) {
             for (uint256 i = 0; i < _tokenIdCounter.current(); i++) {
-                _setTokenURI(i, bearUrisIpfs[0]);
+                _setTokenURI(i, bearUrisIpfs[_randomWords[0] % 3]);
             }
         } else {
             for (uint256 i = 0; i < _tokenIdCounter.current(); i++) {
-                _setTokenURI(i, bullUrisIpfs[0]);
+                _setTokenURI(i, bullUrisIpfs[_randomWords[0] % 3]);
             }
         }
+
+        string memory trend = currentMarketTrend == MarketTrend.BULL ? "bullish" : "bearish";
         emit TokensUpdated(trend);
+        emit RequestFulfilled(_requestId, _randomWords);
     }
 
     function compareStrings(string memory a, string memory b) internal pure returns (bool) {
